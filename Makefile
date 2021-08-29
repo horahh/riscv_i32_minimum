d4edad30bed0e950b077f816535ba504924230ef
Horacio Hidalgo <horacio.hidalgo@gmail.com>
2021-08-28 22:17:55 -0600

    Initialize and display registers in test bench

--- Makefile
+++ Makefile
@@ -1,5 +1,7 @@
 all:
 	iverilog -o riscv32i.out test_bench.v register_file.v
+	vvp riscv32i.out  > riscv32i.vcd
+	gtkwave riscv32i.vcd &
 
 clean:
-	rm -f *.out
+	rm -f *.out; rm -f *.vcd; rm -f *.ltx2
--- test_bench.v
+++ test_bench.v
@@ -25,7 +25,7 @@ module test_register_file(
 	input  [31:0] register_data_2);
 
 	parameter
-	CLOCK_PERIOD = 10;
+	CLOCK_PERIOD = 1;
 
 	reg clock;
 	reg reset;
@@ -43,6 +43,8 @@ module test_register_file(
 
 	initial
 	begin
+		$dumpfile("riscv32i.vcd");
+		$dumpvars(clock);
 		//initialize registers 
 		clock = 0; 
 		reset = 0;
@@ -53,32 +55,32 @@ module test_register_file(
 		register_write_select = 0;
 		register_data_write = 0;
 		// time to read register values
-		#32 ;
+		#66 ;
 		write_enable = 0;
-
-
 		#100 $finish;
+	end
+
+	initial begin
 		// write to registers values from 0 to 31
-		forever @(posedge clock) begin
+		forever @(negedge clock) begin
 			register_write_select +=1;
 			register_data_write +=1;
 		end
+	end
+	initial begin
 		// display all registers in the rs1 output one by one
-		forever @(posedge clock) begin
+		forever @(negedge clock) begin
 			rs1 +=1;
 		end
+	end
 
-
+	initial begin
 		forever
 		@(posedge clock) begin
 			$display( "clock = %b, reset = %b, write_enable = %b", clock, reset, write_enable);
 			$display( "rs1 = 0x%x, rs2 = 0x%x, register_write_select = 0x%x", rs1,rs2, register_write_select);
 			$display( "register_data_write = 0x%x, register_data_1 = 0x%x, register_data_2 = 0x%x", register_data_write, register_data_1, register_data_2);
 		end
-
-	end
-	initial
-	begin
 	end
 endmodule
 
