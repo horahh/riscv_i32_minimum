7d45af700f219e4adbfab3023c198201d6781036
Horacio Hidalgo <horacio.hidalgo@gmail.com>
2021-09-15 21:44:29 -0600

    Add support for r0 to be 0

--- register_file.v
+++ register_file.v
@@ -1,3 +1,8 @@
+
+`define HIGH_IMPEDANCE 32'bz
+`define ZERO 32'b0
+`define ONE  32'b1
+
 module register_file(
 	input  clock,
 	input  reset,
@@ -11,11 +16,16 @@ module register_file(
 
 	reg    [31:0] registers[31:0];
 
-	always@(posedge clock) begin
-		if (write_enable)
+	always@(posedge clock & write_enable) begin
+      if (register_write_select == 0) begin
+			registers[register_write_select] <= `ZERO;
+      end
+      else begin
 			registers[register_write_select] <= register_data_write;
+      end
 	end
 
 	assign register_data_1 = registers[rs1];
 	assign register_data_2 = registers[rs2];
+
 endmodule
