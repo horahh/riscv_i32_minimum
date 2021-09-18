50443e88207a5337d9bda3bfc800ee18e68bf528
Horacio Hidalgo <horacio.hidalgo@gmail.com>
2021-09-18 12:57:18 -0600

    Add beautify to decode.v

--- decode.v
+++ decode.v
@@ -7,6 +7,9 @@ U-type: long immediates.
 J-type: unconditional jumps, a variation of U-type.
 */
 
+`define ZERO 1'b0
+`define ONE  1'b1
+
 module rv32i_decode(
    input      [6:0] opcode
    output reg       branch_type,
@@ -22,29 +25,26 @@ module rv32i_decode(
    output reg       fence_type
 );
 
-   parameter [6:0] BRANCH_TYPE_OPCODE = 0x063; 
-   parameter [6:0] INTEGER_TYPE_JUMP_OPCODE = 0x067; 
-   parameter [6:0] JUMP_TYPE = 0x6f;
-   parameter [6:0] UNCONDITIONAL_TYPE_LOAD = 0x37;
-   parameter [6:0] UNCONDITIONAL_TYPE_ADD = 0x17;
-   parameter [6:0] INTEGER_TYPE_ALU_OPCODE = 0x00C;
-   parameter [6:0] REGISTER_TYPE_ALU_OPCODE = 0x033;
-   parameter [6:0] INTEGER_TYPE_LOAD_OPCODE = 0x003;
-   parameter [6:0] STORE_TYPE_OPCODE = 0x023;
-   parameter [6:0] FENCE_TYPE_OPCODE = 0x0F;
+   parameter [6:0] BRANCH_TYPE_OPCODE       = 7'h63; 
+   parameter [6:0] INTEGER_TYPE_JUMP_OPCODE = 7'h67; 
+   parameter [6:0] JUMP_TYPE                = 7'h6f;
+   parameter [6:0] UNCONDITIONAL_TYPE_LOAD  = 7'h37;
+   parameter [6:0] UNCONDITIONAL_TYPE_ADD   = 7'h17;
+   parameter [6:0] INTEGER_TYPE_ALU_OPCODE  = 7'h0C;
+   parameter [6:0] REGISTER_TYPE_ALU_OPCODE = 7'h33;
+   parameter [6:0] INTEGER_TYPE_LOAD_OPCODE = 7'h03;
+   parameter [6:0] STORE_TYPE_OPCODE        = 7'h23;
+   parameter [6:0] FENCE_TYPE_OPCODE        = 7'h0F;
 
-   branch_type = opcode == BRANCH_TYPE_OPCODE ? `ONE : ZERO;
-   integer_type_jump = opcode == INTEGER_TYPE_JUMP_OPCODE ? `ONE : ZERO;
-   jump_type = opcode == JUMP_TYPE_OPCODE ? `ONE : ZERO;
-   unconditional_type_load = opcode == UNCONDITIONAL_TYPE_LOAD_OPCODE ? `ONE : ZERO;
-   unconditional_type_add = opcode == UNCONDITIONAL_TYPE_ADD_OPCODE ? `ONE : ZERO;
-   integer_type_alu = opcode == INTEGER_TYPE_ALU_OPCODE ? `ONE : ZERO;
-   register_type_alu = opcode == REGISTER_TYPE_ALU_OPCODE ? `ONE : ZERO;
-   integer_type_load = opcode == INTEGER_TYPE_LOAD_OPCODE ? `ONE : ZERO;
-   store_type = opcode == STORE_TYPE_OPCODE ? `ONE : ZERO;
-   fence_type = opcode == FENCE_TYPE_OPCODE ? `ONE : ZERO;
+   branch_type             = opcode == BRANCH_TYPE_OPCODE             ? `ONE : `ZERO;
+   integer_type_jump       = opcode == INTEGER_TYPE_JUMP_OPCODE       ? `ONE : `ZERO;
+   jump_type               = opcode == JUMP_TYPE_OPCODE               ? `ONE : `ZERO;
+   unconditional_type_load = opcode == UNCONDITIONAL_TYPE_LOAD_OPCODE ? `ONE : `ZERO;
+   unconditional_type_add  = opcode == UNCONDITIONAL_TYPE_ADD_OPCODE  ? `ONE : `ZERO;
+   integer_type_alu        = opcode == INTEGER_TYPE_ALU_OPCODE        ? `ONE : `ZERO;
+   register_type_alu       = opcode == REGISTER_TYPE_ALU_OPCODE       ? `ONE : `ZERO;
+   integer_type_load       = opcode == INTEGER_TYPE_LOAD_OPCODE       ? `ONE : `ZERO;
+   store_type              = opcode == STORE_TYPE_OPCODE              ? `ONE : `ZERO;
+   fence_type              = opcode == FENCE_TYPE_OPCODE              ? `ONE : `ZERO;
 
 endmodule
-   
-
-
