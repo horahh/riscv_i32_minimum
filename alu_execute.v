a98108f44bbcf4841b02e8f4c4473938307fd378
Horacio Hidalgo <horacio.hidalgo@gmail.com>
2021-09-18 16:14:38 -0600

    Add execute unit

---
+++ alu_execute.v
@@ -0,0 +1,30 @@
+module alu_execute(
+   input clock,
+   input enable,
+   input [31:0] instruction,
+   output [4:0] rs1,
+   output [4:0] rs2,
+   output [4:0] rd
+);
+
+// split instruction into fields
+// source: https://github.com/jameslzhu/riscv-card/blob/master/riscv-card.pdf
+// core instructions format for R type
+assign funct3 = instruction[14:12];
+assign funct7 = instruction[31:25];
+assign rs1    = instruction[19:15];
+assign rs2    = instruction[24:20];
+
+// connect alu
+
+alu alu_0(
+   .clock(clock),
+   .enable(enable), 
+   .funct3(funct3),
+   .funct7(funct7),
+   .register_data_1(rs1),
+   .register_data_2(rs2),
+   .register_data_out(rd)
+);
+endmodule
+
---
+++ execute.v
@@ -0,0 +1,35 @@
+module execute(
+   input         clock,
+   input  [31:0] instruction,
+   input         branch_type,
+   input         register_type_alu,
+   input         integer_type_jump,
+   input         jump_type,
+   input         unconditional_type_load,
+   input         unconditional_type_add, 
+   input         integer_type_alu,
+   input         register_type_alu,
+   input         integer_type_load,
+   input         store_type,
+   input         fence_type,
+   output        write_back_enable,
+   output [31:0] memory_address,
+   output [31:0] memory_value     
+);
+
+// instantiate register file
+
+// instantiate functional units
+if ( register_type_alu ) begin
+   alu_execute alu_execute_0(
+      .clock(clock), 
+      .enable(register_type_alu), 
+      .instruction(instruction), 
+      .rs1(register_data_1_select),
+      .rs2(register_data_2_select),
+      .rd(register_data_out_select)
+   );
+   
+end
+
+endmodule
