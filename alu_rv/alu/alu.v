29e3ef388e572bf31d9a44971643c8c1ad5efa26
Horacio Hidalgo <horacio.hidalgo@gmail.com>
2021-10-24 01:52:48 -0600

    Fix alu test bench

--- alu_rv/alu/alu.v
+++ alu_rv/alu/alu.v
@@ -11,7 +11,7 @@ module alu(
 
    alu_select alu_select_0(clock, enable, funct7, alu_base_enable, alu_extra_enable);
 
-   alu_base alu_base_0(clock, alu_base_enable, operand_0, operand_1, destination);
-   alu_extra alu_extra_0(clock, alu_extra_enable, operand_0, operand_1, destination);
+   alu_base alu_base_0(clock, alu_base_enable, funct3, operand_0, operand_1, destination);
+   alu_extra alu_extra_0(clock, alu_extra_enable, funct3, operand_0, operand_1, destination);
 
 endmodule
--- alu_rv/alu/test_bench_alu.v
+++ alu_rv/alu/test_bench_alu.v
@@ -7,7 +7,7 @@ module test_bench_alu;
 	wire [31:0] register_data_2;
 	wire [31:0] register_data_out;
 
-	alu_rv alu_0            (clock, enable, funct3, funct7, register_data_1, register_data_2, register_data_out);
+	alu           alu_0     (clock, enable, funct3, funct7, register_data_1, register_data_2, register_data_out);
 
 	test_case_alu test_alu_0(clock, enable, funct3, funct7, register_data_1, register_data_2, register_data_out);
 	
