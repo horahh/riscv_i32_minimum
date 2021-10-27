4ec240a108c7cd95c936a359541207f27f070f98
Horacio Hidalgo <horacio.hidalgo@gmail.com>
2021-10-26 23:03:02 -0600

    Add code to load memory from compiled file

--- memory/memory.v
+++ memory/memory.v
@@ -11,13 +11,22 @@ module memory(
    input  [31:0] write_address,
    input  [31:0] write_value);
 
-   reg [31:0] mem [`MEMORY_SIZE-1:0];
+   reg [31:0] random_access_memory [`MEMORY_SIZE-1:0];
 
    always @(posedge clock) begin
-      assign read_address = read_enable ? mem[read_address] : `HIGH_IMPEDANCE;
+      assign read_address = read_enable ? random_access_memory[read_address] : `HIGH_IMPEDANCE;
    end
    always @(posedge clock & write_enable) begin
-      mem[write_address] = write_value;
+      random_access_memory[write_address] = write_value;
+   end
+
+
+   initial
+   begin
+      $readmemh("../asm_to_hex/code.hex",random_access_memory);
+      forever @(posedge clock)
+         $display("read_enable=%d, read_address=%d, read_value= %d", read_enable, read_address, read_value);
+         $display("write_enable=%d, write_address=%d, write_value=%d", write_enable, write_address, write_value);
    end
 
 endmodule
