39e82bb7aac368c9cb28339bef4a27f025e0611c
Horacio Hidalgo <horacio.hidalgo@gmail.com>
2021-11-14 12:29:30 -0600

    Change register selection for memory simulation

--- asm_to_hex/code.asm
+++ asm_to_hex/code.asm
@@ -9,6 +9,17 @@ add r10, r6, r5
 add r10, r6, r5
 add r10, r6, r5
 add r10, r6, r5
+add r10, r4, r5
+add r10, r6, r5
+add r10, r4, r5
+add r10, r6, r5
+add r10, r4, r5
+add r10, r6, r5
+add r10, r4, r5
+add r10, r6, r5
+add r10, r4, r5
+add r10, r6, r5
+add r10, r4, r5
 sub r3,r1,r0
 add r3, r1, r0
 sub r3,r1,r0
