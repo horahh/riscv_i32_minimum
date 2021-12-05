6e97d81153c5a9e233d8bc12e2b7f74af308d845
Horacio Hidalgo <horacio.hidalgo@gmail.com>
2021-12-04 20:14:37 -0600

    Fix register names in alu register register

--- alu_rv/alu_register_register/alu_register_register.v
+++ alu_rv/alu_register_register/alu_register_register.v
@@ -3,14 +3,35 @@ module alu_register_register(
 	input             alu_register_register_enable,
    input      [2:0]  funct3, 
 	input      [6:0]  funct7,
-   input      [31:0] operand_0,
-   input      [31:0] operand_1,
-   output     [31:0] destination
+   input      [31:0] rs1_value,
+   input      [31:0] rs2_value,
+   output     [31:0] rd_value 
 	);
 
-   alu_select alu_select_0(clock, alu_register_register_enable, funct7, alu_base_enable, alu_extra_enable);
+   alu_select alu_select_0(
+      .clock(clock), 
+      .alu_register_register_enable(alu_register_register_enable), 
+      .funct7(funct7), 
+      .alu_base_enable(alu_base_enable), 
+      .alu_extra_enable(alu_base_enable)
+   );
 
-   alu_base alu_base_0(clock, alu_base_enable, funct3, operand_0, operand_1, destination);
-   alu_extra alu_extra_0(clock, alu_extra_enable, funct3, operand_0, operand_1, destination);
+   alu_base alu_base_0(
+      .clock(clock), 
+      .alu_base_enable(alu_base_enable), 
+      .funct3(funct3), 
+      .rs1_value(rs1_value), 
+      .rs2_value(rs2_value), 
+      .rd_value(rs2_value)
+   );
+
+   alu_extra alu_extra_0(
+      .clock(clock), 
+      .alu_extra_enable(alu_extra_enable), 
+      .funct3(funct3), 
+      .rs1_value(rs1_value),
+      .rs2_value(rs2_value),
+      .rd_value(rd_value)
+   );
 
 endmodule
