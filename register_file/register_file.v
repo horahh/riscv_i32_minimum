09aea4a33708270c6c179f101a9cb9b1b7f1f07d
Horacio Hidalgo <horacio.hidalgo@gmail.com>
2021-12-04 21:18:02 -0600

    Fix register_file connections

--- register_file/register_file.v
+++ register_file/register_file.v
@@ -5,7 +5,7 @@
 module register_file(
 	input         clock,
 	input         reset,
-	input         write_enable,
+	input         register_file_write_enable,
 	input  [4:0]  rs1,
 	input  [4:0]  rs2,
 	input  [4:0]  rd,
@@ -20,11 +20,11 @@ module register_file(
    // define a register stuck at zero apart from the others
    reg    [31:0] register_zero = `ZERO;
 
-	always@(posedge clock & write_enable) begin
+	always@(posedge clock & register_file_write_enable) begin
 			registers[rd] <= rd_value;
 	end
 
-	assign register_data_1 = rs1 ? registers[rs1] : register_zero;
-	assign register_data_2 = registers[rs2];
+	assign rs1_value = rs1 ? registers[rs1] : register_zero;
+	assign rs2_value = registers[rs2];
 
 endmodule
