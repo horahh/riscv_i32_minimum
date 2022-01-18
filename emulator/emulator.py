0a43d6ae04de6ed9be8398c6e9a977e99d2b54cc
Horacio Hidalgo <horacio.hidalgo@gmail.com>
2022-01-18 00:04:44 -0600

    Add factory for emulator

--- emulator/emulator.py
+++ emulator/emulator.py
@@ -12,19 +12,19 @@ def get_args():
         "--asm",
         type=str,
         default="../asm/code.asm",
-        help="Provide the asm to translate to binary",
+        help="Provide the asm to translate to binary.",
     )
     parser.add_argument(
-        "--bin",
+        "--memory",
         type=str,
         default="../bin/code.hex",
-        help="Provide the bin file to dump the binary translation of the provided asm",
+        help="Provide the hex file to load as initial memory into the emulation model.",
     )
     parser.add_argument(
-        "--register_file",
+        "--register",
         type=str,
         default="../default_register_init.hex",
-        help="Provide the hex file to set the initial register file state",
+        help="Provide the hex file to set the initial register file state.",
     )
     args = parser.parse_args()
     return args
@@ -32,9 +32,45 @@ def get_args():
 
 def main():
     args = get_args()
-    instructions = [instruction.Instruction(asm) for asm in args.asm]
-    systemEmulator = SystemEmulator(args.register_file, args.hex)
-    emulator = Emulator(instructions, systemEmulator)
+    emulatorFactory = EmulatorFactory(args.register, args.memory, args.asm)
+    emulator = emulatorFactory.create()
+    emulator.run()
+    # Need to compare results against a reference
+    emulator.get_results()
+
+
+class EmulatorFactory:
+    def __init__(self, register_init_file, memory_init_file, asm_init_file):
+        self.register_init_file = register_init_file
+        self.memory_init_file = memory_init_file
+        self.asm_init_file = asm_init_file
+
+    def create_register_file(self):
+        register_generator = file_read(self.register_init_file)
+        register_file = Register(register_generator)
+        return register_file
+
+    def create_memory(self):
+        memory_generator = file_read(self.memory_init_file)
+        memory = Memory(memory_generator)
+        return memory
+
+    def create_systemEmulator(self, register_file, memory):
+        systemEmulator = SystemEmulator(register_file, memory)
+        return systemEmulator
+
+    def create_instructions(self):
+        asm_generator = file_read(self.asm_init_file)
+        instructions = [instruction.Instruction(asm) for asm in asm_generator]
+        return instructions
+
+    def create(self):
+        register_file = self.create_register_file()
+        memory = self.create_memory()
+        systemEmulator = create_systemEmulator(register_file, memory)
+        instructions = self.create_instructions()
+        emulator = Emulator(instructions, systemEmulator)
+        return emulator
 
 
 class Emulator:
@@ -47,10 +83,15 @@ class Emulator:
     def __init__(self, instructions, systemEmulator):
         self.instructions = instructions
         self.systemEmulator = systemEmulator
+        self.results = []
 
     def run(self):
         for instruction in self.instructions:
             self.systemEmulator.execute(instruction)
+            results.append(self.systemEmulator.dump())
+
+    def get_results(self):
+        return results
 
 
 class SystemEmulator:
@@ -70,12 +111,15 @@ class SystemEmulator:
         """
         Method to dump state of the system at any point in order to verify against HW state.
         """
-        pass
+        register_dump = [register for register in self.registers]
+        return register_dump
 
     def add(self, instruction):
         rs1 = instruction.get_token(2)
         rs2 = instruction.get_token(3)
         rd = instruction.get_token(1)
+        if rd == "r0":
+            return
         registers[rd] = registers[rs1] + registers[rs2]
 
 
@@ -85,9 +129,9 @@ class Memory:
     Load memory verilog compatible hex file having 32 bit values
     """
 
-    def __init__(self, memory_file):
+    def __init__(self, memory_data):
         self.memory = []
-        for line in file_read(register_init_file):
+        for line in memory_data:
             self.memory.append(index)
 
     def __getitem__(self, index):
